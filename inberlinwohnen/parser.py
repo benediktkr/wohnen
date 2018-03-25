d48b8d47d74d2bc4c36c41c9155518aa559f099f
Benedikt Kristinsson <benedikt@inventati.org>
2018-03-26 01:23:09 +0200

    working parser

--- inberlinwohnen/parser.py
+++ inberlinwohnen/parser.py
@@ -1,7 +1,54 @@
+#coding: utf-8
+
 import logging
+
+import urlparse
+import urllib
+from lxml import html
 
 logger = logging.getLogger(__name__)
 
-def parse():
-    logger.debug("test")
-    return "lol"
+def parse(html_input):
+    base_url = "https://inberlinwohnen.de/"
+
+    tree = html.fromstring(html_input)
+    all_flats = tree.xpath("//div[contains(@id,'cflat_')]")
+
+    logger.debug("{} flats found".format(len(all_flats)))
+
+    for flat in all_flats:
+        # Parse HTML
+        adresse = flat.xpath(".//p[@class='adresse']")[0].text_content().strip().split(",")
+        link = flat.xpath(".//a[contains(@title,'Die detailierte')]/@href")[0]
+
+        # The sub-header of Kaltmiete, Wohnflache, Zimmer. Always seems to be the same
+        # and shares the same div class.
+        #
+        # If this breaks: Check the <dd> thing, that tells us which it is. Right now
+        # we're assuming they're always in the same order.
+        maincriteria = [a.text for a in flat.xpath(".//div[@class='maincriteria']/dl/dt")]
+        addrcriteria = [a.text for a in flat.xpath(".//div[@class='addcriteria']/dl/dt")]
+
+        # Unpack values
+        addr, kiez = [a.strip() for a in adresse]
+
+        kalt, sqm, zimmer = maincriteria
+        neben = addrcriteria[0]
+        total = addrcriteria[1]
+        timeframe = addrcriteria[2]
+        floor = addrcriteria[3]
+        year = addrcriteria[5]
+
+        yield {
+            'kalt': kalt.split(" ")[0],
+            'neben': neben.split(" ")[0],
+            'total': total.split(" ")[0],
+            'sqm': sqm.split(" ")[0],
+            'floor': floor,
+            'zimmer': zimmer,
+            'timeframe': timeframe,
+            'year': year,
+            'addr': addr,
+            'kiez': kiez,
+            'link': urllib.quote(urlparse.urljoin(base_url, link), safe=":/"),
+        }
--- search.py
+++ search.py
@@ -20,15 +20,27 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-for site in args.sites:
-    logger.debug(site)
-    sitem = getattr(sys.modules[__name__], site)
-    if args.scrape:
-        scraper = getattr(sitem, "scraper")
-        html = scraper.scrape()
-    else:
-        with open('{}/sample.txt'.format(site), 'r') as f:
-            html = f.read()
+def get_sample(site):
+    with open('{}/sample.txt'.format(site), 'r') as f:
+        ## html will be a list
+        return f.read()
 
-    parser = getattr(sitem, "parser")
-    parser.parse()
+if __name__ == "__main__":
+    for site in args.sites:
+        logger.debug(site)
+        sitem = getattr(sys.modules[__name__], site)
+        if args.scrape:
+            scraper = getattr(sitem, "scraper")
+            html = scraper.scrape()
+        else:
+            scraper = None
+            html = get_sample(site)
+
+        parser = getattr(sitem, "parser")
+
+
+        flats = parser.parse(html)
+        for f in flats:
+            if f['addr'].startswith("Hasen"):
+                import pprint
+                pprint.pprint(f)
