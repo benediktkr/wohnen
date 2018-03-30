cab4900954eb65b4c96030c4cda94a8bad658249
Benedikt Kristinsson <benedikt@inventati.org>
2018-03-30 21:45:40 +0200

    emails to argsparse

--- config.py
+++ config.py
@@ -3,10 +3,6 @@ from logging import DEBUG, INFO, ERROR
 jsonfile = "wohnen-test.json"
 loglevel = DEBUG
 
-email_to = [
-    "benedikt@inventati.org",
-    "eva.catharina@gmail.com"
-]
 
 email_from = "benedikt@sudo.is"
 
